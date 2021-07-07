fee508da493f3b1f1be61e47ef8451ceb418406c
Ben Shalom <filsdepaix060@gmail.com>
2021-07-07 13:03:11 +0530

    Thrird commet

--- example/lib/Dart 1.dart
+++ example/lib/Dart 1.dart
@@ -1,3 +1,14 @@
-void main(){
-  print("Hi ben");
+import 'dart:html';
+
+void main(List<String> args) {
+  print("Hello World");
+}
+class User(){
+String email;//Better to have email of EMAIL type
+String password;//Better to have password of PASSWORD type
+Feed feeddata;
+Chat chat_data;
+Admire admiting_Data;
+
+
 }
