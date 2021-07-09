f1a475baa28d82f52bf6cbeae0e6513faeed1d68
Mohit190801 <mohitpatel4959@gmail.com>
2021-07-09 12:31:47 +0530

    MOHIT commit

---
+++ example/lib/Camera.dart
@@ -0,0 +1,23 @@
+import 'package:camerawesome/camerawesome_plugin.dart';
+import 'package:flutter/material.dart';
+
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
+  late dynamic onull;
+  // [...]
+  // Notifiers
+  ValueNotifier<CameraFlashes> _switchFlash = ValueNotifier(CameraFlashes.NONE);
+  ValueNotifier<Sensors> _sensor = ValueNotifier(Sensors.BACK);
+  ValueNotifier<CaptureModes> _captureMode = ValueNotifier(CaptureModes.PHOTO);
+  ValueNotifier<Size> _photoSize = ValueNotifier(null);
+
+  // Controllers
+  PictureController _pictureController = new PictureController();
+  VideoController _videoController = new VideoController();
+// [...]
+}
